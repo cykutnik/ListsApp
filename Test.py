8fd3c629432fab3a8dff366ab799b11b3231cb4f
cykutnik <julia.barabasz2001@gmail.com>
2022-10-22 12:06:40 +0200

    22.10 database test

--- Test.py
+++ Test.py
@@ -1,10 +1,34 @@
-import mysql.connector
+import mysql.connector as connector
+from getpass import getpass
 
-
-mydb = mysql.connector.connect(
-  host="localhost",
-  user="root",
-  password="Papiezowa!8137"
-)
-
-print(mydb)
+try:
+  with connector.connect(
+    host="localhost",
+    user=input("Wprowadz login: "),
+    password=getpass("Wprowadz haslo: "),
+    database="testowa"
+  ) as connection:
+    #create_db="CREATE DATABASE testowa"
+    #with connection.cursor() as cursor:
+    #  cursor.execute(create_db)
+    # create_lists_table="""
+    # CREATE TABLE lists(
+    #   list_id INT UNSIGNED PRIMARY KEY,
+    #   name VARCHAR(50)
+    # )
+    # """
+    # create_items_table="""
+    # CREATE TABLE items(
+    #   item_id INT UNSIGNED PRIMARY KEY,
+    #   name VARCHAR(50),
+    #   price DECIMAL(10,2) UNSIGNED,
+    #   list_id INT UNSIGNED,
+    #   FOREIGN KEY(list_id) REFERENCES lists(list_id)
+    # )
+    # """
+    with connection.cursor() as cursor:
+      #cursor.execute(create_lists_table) #tu mozna bylo uzyc executemany
+      # cursor.execute(create_items_table)
+      # connection.commit()
+except connector.Error as e:
+  print(e)
